aae3ff206aae798dd1418067fc8b6d7c7bba5386
erickGtz <erick.gutierrezsa@alumno.buap.mx>
2024-09-11 13:58:34 -0600

    Ejercicio 2 añadido

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -1,20 +1,59 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
-<head>
-   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>Practica 4</title>
-</head>
-<body>
-  <h2>Ejercicio 1</h2>
-  <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
 
-  <h4>$_myvar: Valida, debido a que comienza con un guión bajo.</h4>
-  <h4>$_7var: Valida, debido a que comienza con un guión bajo.</h4>
-  <h4>myvar: Invalida, debido a que no comienza con el signo de dolar ($).</h4>
-  <h4>$myvar: Valida, debido a que comienza con letras.</h4>
-  <h4>$var7: Valida, debido a que comienza con letras.</h4>
-  <h4>$_element1: Valida, debido a que comienza con un guión bajo.</h4>
-  <h4>$house*5: Invalida, debido a que tiene el caracter de asterisco. Php toma este caracter como un operador.</h4>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Practica 4</title>
+  </head>
 
-</body>
+  <body>
+
+    <h2>Ejercicio 1</h2>
+    <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
+
+    <p>$_myvar: Valida, debido a que comienza con un guión bajo.</p>
+    <p>$_7var: Valida, debido a que comienza con un guión bajo.</p>
+    <p>myvar: Invalida, debido a que no comienza con el signo de dolar ($).</p>
+    <p>$myvar: Valida, debido a que comienza con letras.</p>
+    <p>$var7: Valida, debido a que comienza con letras.</p>
+    <p>$_element1: Valida, debido a que comienza con un guión bajo.</p>
+    <p>$house*5: Invalida, debido a que tiene el caracter de asterisco. Php toma este caracter como un operador.</p>
+
+    <h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+
+    <p>$a = “ManejadorSQL”; $b = 'MySQL’; $c = &$a;</p>
+
+    <?php
+    $a = "ManejadorSQL";
+    $b = 'MySQL';
+    $c = &$a;
+    ?>
+
+    <p>a. Ahora muestra el contenido de cada variable</p>
+    <?php
+    echo $a;
+    echo $b;
+    echo $c;
+    ?>
+
+    <p>b. Agrega al código actual las siguientes asignaciones:</p>
+    <p>$a = “PHP server”; $b = &$a;</p>
+    <?php
+    $a = "PHP server";
+    $b = &$a;
+    ?>
+
+    <p>c. Vuelve a mostrar el contenido de cada uno</p>
+    <?php
+    echo $a;
+    echo $b;
+    echo $c;
+    ?>
+
+    <p>d. Describe en y muestra en la página obtenida qué ocurrió en el segundo bloque de asignaciones</p>
+  
+
+  </body>
+
 </html>
