880e49d293f4f429285d70dfa0958b60e9ed330c
erickGtz <erick.gutierrezsa@alumno.buap.mx>
2024-09-11 13:49:12 -0600

    Ejercicio 1 añadido

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -5,6 +5,16 @@
   <title>Practica 4</title>
 </head>
 <body>
-  
+  <h2>Ejercicio 1</h2>
+  <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
+
+  <h4>$_myvar: Valida, debido a que comienza con un guión bajo.</h4>
+  <h4>$_7var: Valida, debido a que comienza con un guión bajo.</h4>
+  <h4>myvar: Invalida, debido a que no comienza con el signo de dolar ($).</h4>
+  <h4>$myvar: Valida, debido a que comienza con letras.</h4>
+  <h4>$var7: Valida, debido a que comienza con letras.</h4>
+  <h4>$_element1: Valida, debido a que comienza con un guión bajo.</h4>
+  <h4>$house*5: Invalida, debido a que tiene el caracter de asterisco. Php toma este caracter como un operador.</h4>
+
 </body>
 </html>
