018e91e27b42e42c8c3c402cac6ccd010875c0c0
erickGtz <erick.gutierrezsa@alumno.buap.mx>
2024-09-26 14:24:00 -0600

    Prueba 2

--- practicas/p07/get_productos_xhtml.php
+++ practicas/p07/get_productos_xhtml.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
   <?php
-  header("Content-Type: application/json; charset=utf-8");
   $data = array();
 
   if (isset($_GET['tope'])) {
