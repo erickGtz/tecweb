2380d434c093f5803628d4f67d24b94505db075e
erickGtz <erick.gutierrezsa@alumno.buap.mx>
2024-10-08 23:25:15 -0600

    JS05 pag. 13

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -268,3 +268,27 @@ function mensaje5() {
 
   mostrarRango(valor1, valor2);
 }
+
+//JS05 pag. 13
+function retornarValor() {
+  var div = document.getElementById('retornoValor');
+
+  function convertirCastellano(x) {
+    if (x == 1) return 'uno';
+    else 
+      if (x == 2) return 'dos';
+      else 
+        if (x == 3) return 'tres';
+        else 
+          if (x == 4) return 'cuatro';
+          else 
+            if (x == 5) return 'cinco';
+            else 
+              return 'valor incorrecto';
+  }
+
+  var valor = prompt('Ingresa un valor entre 1 y 5', 0);
+  valor = parseInt(valor);
+  var r = convertirCastellano(valor);
+  div.innerHTML = r;
+}
