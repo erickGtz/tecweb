9a1e93e4e5c9a9c07811c3f80e1eba2cf841ca13
erickGtz <erick.gutierrezsa@alumno.buap.mx>
2024-10-23 12:58:53 -0600

    query busqueda actualizada

--- practicas/p11/p11_con_jquery/product_app/backend/product-search.php
+++ practicas/p11/p11_con_jquery/product_app/backend/product-search.php
@@ -7,7 +7,7 @@
     if( isset($_POST['search']) ) {
         $search = $_POST['search'];
         // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        $sql = "SELECT * FROM productos WHERE (ID = '{$search}' OR nombre LIKE '{$search}%' OR marca LIKE '%{$search}%' OR detalles LIKE '%{$search}%') AND eliminado = 0";
+        $sql = "SELECT * FROM productos WHERE (ID = '{$search}' OR nombre LIKE '{$search}%' OR marca LIKE '{$search}%' OR detalles LIKE '{$search}%') AND eliminado = 0";
         if ( $result = $conexion->query($sql) ) {
             // SE OBTIENEN LOS RESULTADOS
 			$rows = $result->fetch_all(MYSQLI_ASSOC);
