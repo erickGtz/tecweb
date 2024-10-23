76c91b4b6ca33c64aa813b757d43324771e54c00
erickGtz <erick.gutierrezsa@alumno.buap.mx>
2024-10-22 22:18:27 -0600

    Funcion buscar producto v.1

--- practicas/p11/p11_con_jquery/product_app/app.js
+++ practicas/p11/p11_con_jquery/product_app/app.js
@@ -1,22 +1,47 @@
 // JSON BASE A MOSTRAR EN FORMULARIO
 var baseJSON = {
-    "precio": 0.0,
-    "unidades": 1,
-    "modelo": "XX-000",
-    "marca": "NA",
-    "detalles": "NA",
-    "imagen": "img/default.png"
-  };
+  precio: 0.0,
+  unidades: 1,
+  modelo: 'XX-000',
+  marca: 'NA',
+  detalles: 'NA',
+  imagen: 'img/default.png',
+};
 
 function init() {
-    /**
-     * Convierte el JSON a string para poder mostrarlo
-     * ver: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/JSON
-     */
-    var JsonString = JSON.stringify(baseJSON,null,2);
-    document.getElementById("description").value = JsonString;
+  /**
+   * Convierte el JSON a string para poder mostrarlo
+   * ver: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/JSON
+   */
+  var JsonString = JSON.stringify(baseJSON, null, 2);
+  document.getElementById('description').value = JsonString;
 
-    // SE LISTAN TODOS LOS PRODUCTOS
-    listarProductos();
+  // SE LISTAN TODOS LOS PRODUCTOS
+  listarProductos();
 }
 
+$(document).ready(function () {
+  $('#product-result').hide();
+
+  $('#search').keyup(function (e) {
+    let search = $('#search').val();
+    $.ajax({
+      url: 'product-search.php',
+      type: 'POST',
+      data: { search },
+      success: function (response) {
+        let products = JSON.parse(response);
+        let template = '';
+
+        products.forEach((product) => {
+          template += `<li>
+                        ${product.nombre}
+                    </li>`;
+        });
+
+        $('#container').html(template);
+        $('#product-result').show();
+      },
+    });
+  });
+});
