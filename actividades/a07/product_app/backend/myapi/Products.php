fc8baa893688c66e6a4c1265dab85a1f49ac3ccc
erickGtz <erick.gutierrezsa@alumno.buap.mx>
2024-11-06 17:07:04 -0600

    funcion single-byName v.7

--- actividades/a07/product_app/backend/myapi/Products.php
+++ actividades/a07/product_app/backend/myapi/Products.php
@@ -4,49 +4,48 @@ namespace TECWEB\BACKEND\MYAPI;
 
 use TECWEB\BACKEND\MYAPI\DataBase;
 require_once __DIR__ . '/DataBase.php';
+
 class Products extends DataBase
 {
   private $data;
+
   public function __construct($db, $user = 'root', $pass = 'fk1322')
   {
-    $this->data = array();
+    $this->data = '';
     parent::__construct($user, $pass, $db);
   }
 
   public function singleByName($name)
   {
-      // SE REALIZA LA QUERY DE BÚSQUEDA
-      $sql = "SELECT * FROM productos WHERE nombre = $name";
-      $result = $this->conexion->query($sql);
+    // SE REALIZA LA QUERY DE BÚSQUEDA
+    $sql = "SELECT * FROM productos WHERE nombre = '$name'";
+    $result = $this->conexion->query($sql);
 
-      // COMPROBAR SI SE ENCONTRÓ UN PRODUCTO
-      if ($result) {
-        $row = $result->fetch_assoc();
-        $data = array(
-          'ID' => $row['ID'],
-          'nombre' => $row['nombre'],
-          'marca' => $row['marca'],
-          'precio' => $row['precio'],
-          'unidades' => $row['unidades'],
-          'modelo' => $row['modelo'],
-          'detalles' => $row['detalles'],
-          'imagen' => $row['imagen']
-        );
-      } else {
-        // Si no se encuentra el producto, devolvemos un mensaje de error
-        $data = array('error' => 'Producto no encontrado');
-      }
-      // LIBERAR EL RESULTADO
+    // COMPROBAR SI SE ENCONTRÓ UN PRODUCTO
+    if ($result && $row = $result->fetch_assoc()) {
+      $data = array(
+        'ID' => $row['ID'],
+        'nombre' => $row['nombre'],
+        'marca' => $row['marca'],
+        'precio' => $row['precio'],
+        'unidades' => $row['unidades'],
+        'modelo' => $row['modelo'],
+        'detalles' => $row['detalles'],
+        'imagen' => $row['imagen']
+      );
+    } else {
+      // Si no se encuentra el producto, devolvemos un mensaje de error
+      $data = array('error' => 'Producto no encontrado');
+    }
 
-      // CERRAR LA CONEXIÓN
-      $this->conexion->close();
-    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
+    // Asignar el JSON a $this->data en lugar de imprimirlo directamente
+    $this->data = json_encode($data, JSON_PRETTY_PRINT);
+
+    // LIBERAR EL RESULTADO Y CERRAR LA CONEXIÓN
+    $this->conexion->close();
   }
 
-  public function getData(): string{
+  public function getData(): string {
     return $this->data;
   }
-
 }
-?>
