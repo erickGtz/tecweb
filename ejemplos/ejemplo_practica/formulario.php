84294c8487c26c1cef982c1dcb8b405248c92697
erickGtz <erick.gutierrezsa@alumno.buap.mx>
2024-11-08 15:49:43 -0600

    Carpeta ejemplos: agregada

---
+++ ejemplos/ejemplo_practica/formulario.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style type="text/css">
+      ol, ul { 
+      list-style-type: none;
+      }
+    </style>
+    <title>Formulario</title>
+</head>
+<body>
+    <h1>Datos Personales</h1>
+
+    <form id="miFormulario" onsubmit="" method="post">
+        <fieldset>
+            <legend>Actualiza los datos personales de esta persoa:</legend>
+            <ul>
+                <li>
+                    <label>Nombre:</label>
+                    <input type="text" name="name" value="<?= isset($_POST['nombre']) ? htmlspecialchars($_POST['nombre']) : '' ?>">
+                </li>
+                <li>
+                    <label>Edad:</label>
+                    <input type="text" name="age" value="<?= isset($_POST['edad']) ? htmlspecialchars($_POST['edad']) : '' ?>">
+                </li>
+            </ul>
+        </fieldset>
+        <p>
+            <input type="submit" value="ENVIAR">
+        </p>
+    </form>
+</body>
+</html>
